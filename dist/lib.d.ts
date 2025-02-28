800e61e1ad7975d127903b1e8db46304e37987c4
Frantzly Dumeny <diegotus1992@gmail.com>
2025-02-28 17:05:20 -0500

    publish dist

---
+++ dist/lib.d.ts
@@ -0,0 +1,2 @@
+import { FilterArgs } from "./typings";
+export declare function handleSearchData(filter: FilterArgs): Record<string, any>;
---
+++ dist/typings.d.ts
@@ -0,0 +1,26 @@
+declare enum EmptyEnum {
+    null = 0,
+    notNul = 1
+}
+declare enum Sort {
+    null = 0,
+    notNul = 1
+}
+export interface FilterArgs {
+    columns?: Columns[];
+    dateRange?: DateRangeOptions;
+    or: Columns[];
+    and: Columns[];
+}
+export interface DateRangeOptions {
+    from?: string;
+    to?: string;
+    name: string;
+}
+export interface Columns {
+    name: string;
+    search?: EmptyEnum | string | string[] | number | number[];
+    orderBy?: Sort;
+}
+export declare function isEmptyEnum(value: string): boolean;
+export {};
